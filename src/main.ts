0a64148fd09d4760e6405f9b228cae47dfb62c09
Vincent Bernet <vincent.bernet99@gmail.com>
2022-02-07 23:01:34 +0100

    fix: deleting some files for heroku host

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   };*/
   const app = await NestFactory.create(AppModule /*httpsOptions*/);
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
