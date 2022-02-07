a2be7c6037d6aa3ad7e505eaed6e54fe81a9da80
Vincent Bernet <vincent.bernet99@gmail.com>
2022-02-07 19:20:50 +0100

    :bug: fix the call of external local certificate

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,11 @@ import { AppModule } from './app.module';
 import fs = require('fs');
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('./../../ressource/localhost-key.pem'),
-    cert: fs.readFileSync('./../../ressource/localhost.pem'),
-  };
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
+  /*const httpsOptions = {
+    key: fs.readFileSync('./../../ressource/localhost-key.pem') || '',
+    cert: fs.readFileSync('./../../ressource/localhost.pem') || '',
+  };*/
+  const app = await NestFactory.create(AppModule /*httpsOptions*/);
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
