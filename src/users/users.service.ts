0cf9b5273c7aef348e1bf8920a8690be1eb1c3b6
Vincent Bernet <vincent.bernet99@gmail.com>
2022-03-14 22:33:36 +0100

    feat: update jest config

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
-import { hashPassword } from 'src/auth/hash';
+import { hashPassword } from './../auth/hash';
 
 @Injectable()
 export class UsersService {
